fdfc5012b6c083a999ceb6cd60f9324a6d23643a
Arkadiusz Piotrowski <arek.gabrys@gmail.com>
2017-01-06 11:13:52 +0100

    Initial Version

---
+++ nunitphantom/Page.cs
@@ -0,0 +1,105 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nunitphantom
+{
+    [TestFixture]
+    public class Page
+    {
+        private static int WaitTime = 5;
+        protected PhantomJSDriver driver;
+
+        public string Url { get; set; }
+
+        [OneTimeSetUp]
+        public void ClassSetup()
+        {
+            driver = new PhantomJSDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 40));
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            if (string.IsNullOrEmpty(Url))
+                throw new Exception("URL Cannot be empty");
+
+            driver.Navigate().GoToUrl(Url);
+            driver.ExecuteScript("$.fx.off = true");
+        }
+
+        [OneTimeTearDown]
+        public void ClassTearDown()
+        {
+            driver.Quit();
+        }
+
+        protected string CombineCssSelector(string _base, string _selector)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendFormat("{0} {1}", _base, _selector);
+            return builder.ToString();
+        }
+
+        protected void ScreenShot()
+        {
+            Screenshot sh = driver.GetScreenshot();
+            sh.SaveAsFile(@"C:\Temp.jpg", ImageFormat.Png);
+        }
+
+        public void ClickById(string elemId)
+        {
+            var domElement = FindElementById(elemId);
+            domElement.Click();
+        }
+
+        public void ClickByCss(string elemCss)
+        {
+            var domElement = getWait().Until(driver => driver.FindElement(By.CssSelector(elemCss)));
+            domElement.Click();
+        }
+
+        public void ClickByXpath(string elemXPath)
+        {
+            var domElement = this.FindElementByXpath(elemXPath);
+            domElement.Click();
+        }
+        
+        public IWebElement FindElementByXpath(string XPath)
+        {
+            return getWait().Until(driver => driver.FindElement(By.XPath(XPath)));
+        }
+
+        public IWebElement FindElementById(string elemId)
+        {
+            return getWait().Until(driver => driver.FindElement(By.Id(elemId)));
+        }
+
+        public IWebElement FindElementByCssSelector(string selector)
+        {
+            return getWait().Until(driver => driver.FindElement(By.CssSelector(selector)));
+        }
+
+        public void ChooseValueInSelectById(string elemId, string value)
+        {
+            var select = new SelectElement(FindElementById(elemId));
+            select.SelectByValue(value);
+        }
+
+        private WebDriverWait getWait()
+        {
+            return new WebDriverWait(new SystemClock(), 
+                driver, 
+                TimeSpan.FromSeconds(WaitTime), 
+                TimeSpan.FromMilliseconds(500) );
+        }
+    }
+}
---
+++ nunitphantom/PostCodeTests.cs
@@ -0,0 +1,102 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace nunitphantom
+{
+    [Category("PostCodeValidation")]
+    public class PostCodeTests : Page
+    {
+        #region Private
+        private string postCodeGroup = ".your-postcode";
+        private string postCodeField = "postcode-field";
+        private string compareButton = "compare-button";
+        private string gasButton = "//*[@id=\"splash\"]/div/div/div/div/div/div[1]/div[2]/div/div[2]/label";
+        private string postCodeError = "//*[@id=\"splash\"]/div/div/div/div/div/div[2]/div/div/div[2]/ul/li";
+        private string mpanAlertSelector = ".q-mpan .alert.alert-info";
+
+        private string multipleMpansAddressValue = "2041496013d57b4d0828f0603dd6b9919670e8c3";
+        private string firstAddressInMultipleMpansXpath = "//*[@id=\"multiple_meters\"]/div[1]/label/input";
+        private string formContainerId = "form-container";
+        private string addressFieldId = "address-field";
+
+        private string wrongPostCode = "123";
+        private string correctPostCode = "oX169BX";
+        private string multipleMpansPostCode = "ky7 6lq";
+        #endregion
+
+        public PostCodeTests()
+        {
+            this.Url = "http://business.ukpower.co.uk/";
+        }
+
+        [Test]
+        public void Entering_PostCode_Lower_Should_Replace_To_Upper()
+        {
+            EnterPostcode(multipleMpansPostCode);
+            var elem = FindElementById(postCodeField);
+            var upperCasePostCode = elem.GetAttribute("value");
+
+            Assert.AreEqual("KY7 6LQ", upperCasePostCode);
+        }
+
+        [Test]
+        public void InCorrect_PostCode_Error_Visible()
+        {
+            EnterPostcode(wrongPostCode, 50);
+
+            Assert.AreEqual(true, FindElementByXpath(postCodeError).Displayed);
+        }
+
+        [Test]
+        public void Correct_PostCode_Error_Invisible()
+        {
+            EnterPostcode(correctPostCode);
+
+            Assert.AreEqual(false, FindElementByXpath(postCodeError).Displayed);
+        }
+
+        [Test]
+        public void Correct_PostCode_Energy_Banner_Visible()
+        {
+            EnterPostcode(correctPostCode);
+
+            Assert.AreEqual(true, FindElementById(formContainerId).Displayed);
+        }
+
+        [Test]
+        public void Selecting_Multiple_Mpans_Noticed_Visible()
+        {
+            EnterPostcode(multipleMpansPostCode);
+            ChooseValueInSelectById(addressFieldId, multipleMpansAddressValue);
+
+            Assert.AreEqual(true, FindElementByCssSelector(mpanAlertSelector).Displayed);
+        }
+
+        [Test]
+        public void Selecting_First_Mpan_In_Multiple_Mpans_Noticed_InVisible()
+        {
+            EnterPostcode(multipleMpansPostCode);
+            ChooseValueInSelectById(addressFieldId, multipleMpansAddressValue);
+            ClickByXpath(firstAddressInMultipleMpansXpath);
+
+            Assert.AreEqual(false, FindElementByCssSelector(mpanAlertSelector).Displayed);
+        }
+
+        private void EnterPostcode(string _postCode, int sleep=2000)
+        {
+            var postcode = FindElementById(postCodeField);
+            postcode.SendKeys(_postCode);
+
+            ClickById(compareButton);
+            Thread.Sleep(sleep);
+
+        }
+    }
+}
